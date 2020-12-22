e588aa911c921cd94d6d51e6238dc95173230481
Holubanastasiia <Holubanastasiia@gmail.com>
2020-12-22 15:53:43 +0300

    cw1
    Пока готово 3 задания по классной работе.

---
+++ cw1/js/task2.js
@@ -0,0 +1,6 @@
+// Task 2
+//         Запросить у пользователя имя.
+//         Вывести сообщение "Добрый день," + имя
+
+var question = prompt("What is your name?");
+document.write("Good to see you, " + question);
