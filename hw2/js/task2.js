da52ab31cc5685292737da416d3b6bd2b652934f
Holubanastasiia <Holubanastasiia@gmail.com>
2020-12-26 15:12:12 +0300

    hw2

---
+++ hw2/js/task2.js
@@ -0,0 +1,26 @@
+// 2. Построить прямоугольный треугольник состоящий из символа "*".
+
+    for (var i = 0; i < 1; i++) {         
+        for (var j = 0; j < 4; j++) {
+            document.write("* ");   
+        }
+         document.write("<br/>");
+}
+    for (var i = 0; i < 1; i++) {         
+        for (var j = 0; j < 3; j++) {
+            document.write("* ");   
+        }
+         document.write("<br/>");
+}
+    for (var i = 0; i < 1; i++) {         
+        for (var j = 0; j < 2; j++) {
+            document.write("* ");   
+        }
+         document.write("<br/>");
+}
+    for (var i = 0; i < 1; i++) {         
+        for (var j = 0; j < 1; j++) {
+            document.write("* ");   
+        }
+         document.write("<br/>");
+}
