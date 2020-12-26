350a973f096212d5b88e26c780a35500eadbceee
Holubanastasiia <Holubanastasiia@gmail.com>
2020-12-26 15:32:33 +0300

    hw2

--- hw2/js/task1.js
+++ hw2/js/task1.js
@@ -2,9 +2,9 @@
 
 var number1, number2, number3, average; 
 
-number1 = +prompt('Введите первое число');
-number2 = +prompt('Введите второе число');
-number3 = +prompt('Введите третье число');
+number1 = +prompt("Введите первое число");
+number2 = +prompt("Введите второе число");
+number3 = +prompt("Введите третье число");
 average = (number1 + number2 + number3) / 3;
 
 if (number1 === number2 || number1 === number3 || number2 === number3) {
--- hw2/js/task3.js
+++ hw2/js/task3.js
@@ -1,1 +1,14 @@
 //  3. Определить какое из трех, введенных пользователем, чисел максимальное и вывести его на экран.
+
+var number1, number2, number3;
+
+number1 = +prompt("Введите первое число");
+number2 = +prompt("Введите второе число");
+number3 = +prompt("Введите третье число");
+
+if (number1 > number2 && number1 > number3) {
+    document.write(number1);
+} else if (number2 > number3 && number2 > number1) {
+    document.write(number2);
+} else (number3 > number1 && number3 > number2) 
+    document.write(number3);
