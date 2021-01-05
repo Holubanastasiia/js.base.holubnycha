ce95709074c0c76489534da6cae9eb0f666cfb49
Holubanastasiia <Holubanastasiia@gmail.com>
2021-01-05 15:30:16 +0300

    рц4

--- hw4/js/task1.js
+++ hw4/js/task1.js
@@ -3,17 +3,14 @@
 // 		Функция должна проверять корректность введенных чисел и знака операции.
 // 		Все аргументы для функции принять от пользователя.
 function calculate(a, b, sign) {
-
     if (isNaN(a) || isNaN(b)) {
         document.write("Вы ввели некорректное число!");
         return;
     }
-
     var operand1 = +prompt("Введите первое число: ");
     var sign = prompt("Введите знак арифметической операции: + - * /");
     var operand2 = +prompt("Введите второе число: ");
     var result;
-
     switch (sign) {
         case "+":
             result = add(operand1, operand2);
@@ -30,7 +27,8 @@ function calculate(a, b, sign) {
         default:
             document.write("<p><b style='color:red'>" + sign + "</b> - не является знаком арифметической операции.</p>");
     }
-}
-        if (result != undefined) {
+     if (result != undefined) {
             document.write("Результат операции: " + result);
         }
+}
+       
