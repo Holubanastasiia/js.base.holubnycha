1626a210e80220635d85f9aedb26a05b381acab1
Holubanastasiia <Holubanastasiia@gmail.com>
2021-01-05 17:37:02 +0300

    HW4

--- hw4/js/task1.js
+++ hw4/js/task1.js
@@ -3,42 +3,41 @@
 // 		Функция должна проверять корректность введенных чисел и знака операции.
 // 		Все аргументы для функции принять от пользователя.
 //  
-   
+
 function calculate(a, b, sign) {
-        var result;
-        if (isNaN(a) || isNaN(b) )
-        {
-            alert("Вы ввели некорректное число!");
+    var result;
+    if (isNaN(a) || isNaN(b)) {
+        alert("Вы ввели некорректное число!");
+        return;
+    }
+    switch (sign) {
+        case "+": {
+            result = a + b;
+            break;
+        }
+        case "-": {
+            result = a - b;
+            break;
+        }
+        case "/": {
+            result = a / b;
+            break;
+        }
+        case "*": {
+            result = a * b;
+            break;
+        }
+        default: {
+            document.write("<p><b style='color:red'>" + sign + "</b> - не является знаком арифметической операции.</p>");
             return;
         }
-        switch (sign) {
-            case "+" : {
-                result = a + b;
-                break;
-            }
-            case "-" : {
-                result = a - b;
-                break;
-            }
-            case "/" : {
-                result = a / b;
-                break;
-            }
-            case "*" : {
-                result = a * b;
-                break;
-            }
-            default: {
-                document.write("<p><b style='color:red'>" + sign + "</b> - не является знаком арифметической операции.</p>");
-                return;
-            }
-        }
-            return result;
+    }
+    return result;
 }
 var a = +prompt("Введите первое число: ");
 var sign = prompt("Введите знак арифметической операции: + - * /");
 var b = +prompt("Введите второе число: ");
 var result = calculate(a, b, sign);
 if (result != undefined) {
-        document.write("Результат операции: " + result);
+    document.write("Результат операции: " + result);
 }
