2e1a04559d280cd0473b27a304ed683a05dea6ce
Holubanastasiia <Holubanastasiia@gmail.com>
2021-01-05 16:26:40 +0300

    hw4

--- hw4/js/task1.js
+++ hw4/js/task1.js
@@ -2,33 +2,42 @@
 // 		Функция должна расчитывать результат операции, исходя из переданного ей знака. 
 // 		Функция должна проверять корректность введенных чисел и знака операции.
 // 		Все аргументы для функции принять от пользователя.
+ var operand1 = +prompt("Введите первое число: ");
+var sign = prompt("Введите знак арифметической операции: + - * /");
+var operand2 = +prompt("Введите второе число: ");
+var result; 
+
 function calculate(a, b, sign) {
     if (isNaN(a) || isNaN(b)) {
         document.write("Вы ввели некорректное число!");
         return;
     }
-    var operand1 = +prompt("Введите первое число: ");
-    var sign = prompt("Введите знак арифметической операции: + - * /");
-    var operand2 = +prompt("Введите второе число: ");
-    var result;
+
+    calculate(operand1, operand2, sign);
+
     switch (sign) {
-        case "+":
-            result = add(operand1, operand2);
+        case "+": {
+            result = operand1 + operand2;
             break;
-        case "-":
-            result = sub(operand1, operand2);
-            break;
-        case "*":
-            result = mul(operand1, operand2);
-            break;
-        case "/":
-            result = div(operand1, operand2);
-            break;
-        default:
-            document.write("<p><b style='color:red'>" + sign + "</b> - не является знаком арифметической операции.</p>");
-    }
-     if (result != undefined) {
-            document.write("Результат операции: " + result);
         }
+        case "-": {
+            result = operand1 - operand2;
+            break;
+        }
+        case "/": {
+            result = operand1 / operand2;
+            break;
+        }
+        case "*": {
+            result = operand1 * operand2;
+            break;
+        }
+        default: {
+            document.write("<p><b style='color:red'>" + sign + "</b> - не является знаком арифметической операции.</p>");
+            return;
+        }
+    }
+    if (result != undefined) {
+        document.write("Результат операции: " + result);
+    }
 }
-       
