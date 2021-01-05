7bdec8764366076ecc2777398b5f2e82c012f773
Holubanastasiia <Holubanastasiia@gmail.com>
2021-01-05 21:29:12 +0300

    hw4

--- hw4/js/task2.js
+++ hw4/js/task2.js
@@ -1,14 +1,18 @@
 // 2.	Создать функцию, возводящую число в степень, число и сама степень вводится пользователем
 // Проверить числа на корректность
 
-
 var number = +prompt("Введите число.");
 var degree = +prompt("Введите в какую степень Вы хотите возвести число.");
-var result = Math.pow(number, degree);
-function test() {
-    alert(result);
+
+function test(a, b) {
+    var result = 1;
+    if (isNaN(a) || isNaN(b)) {
+        console.log("Вы ввели некорректное число или степень!");
+    }
+    for (let i = 0; i < b; i++) {
+        result *= a;
+    }
+    return result;
 }
-if (isNaN(number) || isNaN(degree)) {
-    alert("Вы ввели некорректное число или степень!");
-}
-test()
+console.log(test(number, degree))
+
