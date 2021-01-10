b4271d73c2aaec5c1fcabe24f94ecc3e29391e87
Holubanastasiia <Holubanastasiia@gmail.com>
2021-01-10 13:31:13 +0300

    hw5

--- hw5/js/task2.js
+++ hw5/js/task2.js
@@ -1,3 +1,42 @@
 // 2. Создать объект Calculator, который должен содержать методы для расчета суммы, разности, произведения и частного двух чисел.
 // Протестировать данный объект, принимая от пользователя значения двух операндов и знак операции.
 // В зависимости от принятого знака операции, вызвать соответствующий метод.
+
+var a = +prompt("Введите первое число: ");
+var sign = prompt("Введите знак арифметической операции: + - * /");
+var b = +prompt("Введите второе число: ");
+if (isNaN(a) || isNaN(b)) {
+    console.log("Вы ввели некорректное число!");
+}
+var calculator = {
+    sum: function (a, b) {
+        var result = a + b;
+        console.log(result);
+    },
+    sub: function (a, b) {
+        var result = a - b;
+        console.log(result);
+    },
+    mul: function (a, b) {
+        var result = a * b;
+        console.log(result);
+    },
+    div: function (a, b) {
+        var result = a / b;
+        console.log(result);
+    }
+}
+
+if (sign == "+") {
+    calculator.sum(a, b);
+} else if (sign == "-") {
+    calculator.sub(a, b);
+} else if (sign == "*") {
+    calculator.mul(a, b);
+} else if (sign == "/") {
+    calculator.div(a, b);
+}
+
+
+
+
--- hw5/js/task3.js
+++ hw5/js/task3.js
@@ -1,3 +1,46 @@
 // 3. Создать массив, который будет хранить в себе информацию о сотрудниках предприятия.Каждый элемент масива — объект,
 //     содержащий свойства: name, sName, age, occupation, и метод show, который выводит всю информацию о пользователе.
 // Реализовать заполнение массива пользователем.По окончанию заполнения — вывести информацию о сотрудниках.
+
+var staff = [
+    cooperator1 = {
+        name: a = prompt("Введите имя сотрудника"),
+        sName: b = prompt("Введите фамилию сотрудника"),
+        age: c = prompt("Введите вoзраст сотрудника"),
+        occupation: d = prompt("Введите должность сотрудника"),
+        show: function () {
+            console.log("Имя сотрудника1: " + this.name);
+            console.log("Фамилия сотрудника1: " + this.sName);
+            console.log("Возраст сотрудника1: " + this.age);
+            console.log("Должность сотрудника1: " + this.occupation);
+        }
+    },
+    cooperator2 = {
+        name: a = prompt("Введите имя сотрудника"),
+        sName: b = prompt("Введите фамилию сотрудника"),
+        age: c = prompt("Введите вoзраст сотрудника"),
+        occupation: d = prompt("Введите должность сотрудника"),
+        show: function () {
+            console.log("Имя сотрудника2: " + this.name);
+            console.log("Фамилия сотрудника2: " + this.sName);
+            console.log("Возраст сотрудника2: " + this.age);
+            console.log("Должность сотрудника2: " + this.occupation);
+        }
+    },
+    cooperator3 = {
+        name: a = prompt("Введите имя сотрудника"),
+        sName: b = prompt("Введите фамилию сотрудника"),
+        age: c = prompt("Введите взраст сотрудника"),
+        occupation: d = prompt("Введите должность сотрудника"),
+        show: function () {
+            console.log("Имя сотрудника3: " + this.name);
+            console.log("Фамилия сотрудника3: " + this.sName);
+            console.log("Возраст сотрудника3: " + this.age);
+            console.log("Должность сотрудника3: " + this.occupation);
+        }
+    }
+]
+
+for (var i = 0; i < staff.length; i++) {
+    staff[i].show();
+}
